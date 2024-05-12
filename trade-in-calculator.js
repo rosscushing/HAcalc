a3fca13e7fa281685994521a1e04a022119d5765
rosscushing <ross.cushing@livebetterhearing.com>
2024-05-11 20:00:51 -0400

    Update trade-in-calculator.js

--- trade-in-calculator.js
+++ trade-in-calculator.js
@@ -13,24 +13,30 @@ function calculateTradeInValue() {
 
     // Detailed logic to handle the calculation based on the input
     if (purchaseSource === 'direct') {
-        if (purchasePrice > 2500) {
+        if (purchasePrice >= 2500) {
             if (age < 3) {
                 tradeInValue = 500;
             } else if (age >= 3 && age <= 4) {
                 tradeInValue = 300; // Adjusted for clarity to include devices exactly 3 years old
             }
+            } else if (age > 4 && age <= 5) {
+                tradeInValue = 200; 
+            }
         } else if (purchasePrice >= 1800 && purchasePrice <= 2499) {
-            if (age < 3) {
+            if (age < 2) {
                 tradeInValue = 400;
+            } else if (age >= 2 && age <= 3) {
+                tradeInValue = 300;
+            }
             } else if (age >= 3 && age <= 4) {
                 tradeInValue = 200;
             }
         } else if (purchasePrice >= 1300 && purchasePrice <= 1799 && age < 3) {
-            tradeInValue = 300;
+            tradeInValue = 275;
         }
-    } else if (age < 3) {
-        tradeInValue = 200;
-    }
+            } else if (age >= 3 && age <= 4) {
+                tradeInValue = 175;
+            }
 
     document.getElementById('result').innerText = `Trade-In Value: $${tradeInValue}`;
 }
